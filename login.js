a9c74cf2ccfd02e5a729cd4ef43c2cbbad0d5cdd
FranAlcan <113629064+FranAlcan@users.noreply.github.com>
2022-12-08 23:01:21 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,29 @@
+
+let usu=(prompt("ingrese el usuario"));
+let con1=(prompt("ingrese la contraseña"));
+
+
+if(usu=="usuario1" && con1=="132")
+{
+    alert ("Bienvenido");
+}
+
+else
+{
+ 
+   alert ("Usuario o contrseña incorecta (intentos restantes: 1)")
+
+   let usu=(prompt("ingrese el usuario"));
+let con1=(prompt("ingrese la contraseña"));
+
+
+if(usu=="usuario1" && con1=="132")
+{
+    alert ("Bienvenido");
+
+    
+}
+
+    
+}
+
